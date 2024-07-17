34adbc5274c36040a3ba5f62700377acc1c93579
0xweatherreport <39281290+0xweatherreport@users.noreply.github.com>
2024-07-17 21:28:00 +0000

    move apt package installs from dockerfile to init.sh script

---
+++ .devcontainer/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+# Switch to the .devcontainer folder
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Install packages
+sudo apt-get update
+sudo apt-get install -y iputils-ping
+sudo apt install -y nmap
+sudo apt-get install -y telnet
+sudo apt-get install -y ftp
+sudo apt-get install -y smbclient
+sudo apt install -y redis-tools
+sudo apt-get install -y lynx
